66e533032b1200160d5050031c27f63dfac7dc44
Willem Adnet <willem.adnet@grenoble-inp.org>
2025-03-03 00:40:09 +0100

    test du modele qui n'est clairememnt pas concluant. A voir comment la'meliorer

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 PYTHON = python3.13
 SRC = src
-FILE = main.py
 FILELEARN = firstLearn.py
+PREDICTION=prediction.py
+CREATION=creationModel
+UTILISATION=utilisationModel
 
 all: run
 
-run: 
-	$(PYTHON) $(SRC)/$(FILE)
+run:
+	$(PYTHON) $(SRC)/$(CREATION)/$(FILELEARN)
 
-learn:
-	$(PYTHON) $(SRC)/$(FILELEARN)
+prediction:
+	$(PYTHON) $(SRC)/$(UTILISATION)/$(PREDICTION)
 
 open_data:
 	open ./data/cifar-10-batches-py/readme.html
--- src/utilisationModel/prediction.py
+++ src/utilisationModel/prediction.py
@@ -2,11 +2,48 @@
 
 import torch
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 from PIL import Image
 import sys
 from chargementModele import chargementModel
+from subprocess import run
 
+IMAGE_PATH_FORMAT = "src/data/images/"
 IMAGE_PATH = "src/data/images/image-cinq.jpg"
+
+def foundInfo(format):
+    listElem = format.split("-")
+    try:
+        expected, pos = int(listElem[2]), int(listElem[3][:-4])
+        return expected, pos
+    except ValueError :
+        print("erreur conversion")
+        return None, None
+
+def goodFormat(elem):
+    format = elem.split("-")
+    return len(format) == 4 and format[0] == "pixil" and format[1] == "frame" 
+
+def foundImages():
+    output = run(["ls", "src/data/images"], capture_output=True)
+    listImage = {}
+    if output.returncode == 0:
+        for elem in output.stdout.decode().split('\n'):
+            if goodFormat(elem):
+                numExpected, pos = foundInfo(elem)
+                if numExpected is None:
+                    continue
+                if numExpected not in listImage:
+                    listImage[numExpected] = [(elem, pos)]
+                else:
+                    listImage[numExpected].append((elem, pos))
+    return listImage
+
+def affiche(path):
+    image = Image.open(path)
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
 
 def load_and_preprocess_image(image_path, is_grey):
     """
@@ -41,7 +78,27 @@ def predict(image_path):
     
     print(f"Le modèle prédit : {predicted.item()}")
 
+def predictDict(dicoImages):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    model, is_grey = chargementModel()
+
+    for key in dicoImages:
+        for tupleTitlePos in dicoImages[key]:
+            pathImage, pos = tupleTitlePos
+            image = load_and_preprocess_image(IMAGE_PATH_FORMAT + pathImage, is_grey)
+            image = image.to(device)
+
+            with torch.no_grad():
+                output = model(image)
+                _, predicted = torch.max(output, 1)
+            
+            print(f"Le modèle prédit : {predicted.item()} et la valeur attendu est {key}")
+
+
 if __name__ == "__main__":
+    dicoImages = foundImages()
     if len(sys.argv) > 1:
         IMAGE_PATH = sys.argv[1]
     predict(IMAGE_PATH)
+    predictDict(dicoImages)
